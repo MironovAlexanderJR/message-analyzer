66db7cbf747651fb6755f02440296c62d3af54cc
Miron <alexander.mironov.jr@ya.ru>
2022-11-09 17:41:05 +0300

    exam

--- core/src/main/java/liga/medical/messageanalyzer/core/controller/RabbitController.java
+++ core/src/main/java/liga/medical/messageanalyzer/core/controller/RabbitController.java
@@ -1,13 +1,10 @@
 package liga.medical.messageanalyzer.core.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import liga.medical.commondto.DebugLogDto;
 import liga.medical.messageanalyzer.core.api.RabbitSenderService;
-import liga.medical.messageanalyzer.core.clients.CommonClient;
 import liga.medical.messageanalyzer.core.config.RabbitConfig;
 import liga.medical.messageanalyzer.dto.RabbitMessageFirstDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RabbitController {
 
     private final RabbitSenderService rabbitSenderService;
-    private final CommonClient commonClient;
 
     @PostMapping("/send")
     public void sendMessage(@RequestBody RabbitMessageFirstDto messageDto) throws JsonProcessingException {
